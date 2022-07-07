ed27dff3bcb3c588225d5c773137ad6e293a2f8f
jorgelinadev <jorgelinalberti@gmail.com>
2022-07-07 12:08:44 -0300

    first commit

---
+++ app.js
@@ -0,0 +1,9 @@
+//alert ("Mi archivo está bien linkeado")
+
+const dias = prompt ('Ingresa un valor en días');
+
+const resultado = dias *24 *60 * 60;
+
+alert ('El valor ingresado en días: ' + dias +', equivale a: '+ resultado + ' segundos.');
+
+console.log (alert);
